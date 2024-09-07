691ea90f4d8ce94a053f0eba416b416568da5296
pwfff <954340+pwfff@users.noreply.github.com>
2024-09-07 13:56:26 -0500

    add pattern/example for easy message handling using function callbacks (#1)

---
+++ examples/herokilled/src/handler.rs
@@ -0,0 +1,163 @@
+use std::marker::PhantomData;
+
+use haste::{entities::UpdateType, fieldvalue::FieldValue, fxhash};
+
+use crate::{
+    entities,
+    parser::{self, Context, Visitor},
+};
+
+pub trait PlayerState {
+    fn set_player(&mut self, player: Player);
+    fn get_player(&self, slot: &i32) -> Option<&Player>;
+}
+
+pub trait MessageHandler<S: PlayerState, M: prost::Message + Default> {
+    fn handle(&self, state: &mut S, ctx: &Context, message: &M) -> parser::Result<()>;
+}
+
+impl<
+        S: PlayerState,
+        M: prost::Message + Default,
+        F: Fn(&mut S, &Context, &M) -> parser::Result<()>,
+    > MessageHandler<S, M> for F
+{
+    fn handle(&self, state: &mut S, ctx: &Context, message: &M) -> parser::Result<()> {
+        self(state, ctx, message)
+    }
+}
+
+trait Handler<S> {
+    fn handle(
+        &mut self,
+        state: &mut S,
+        ctx: &Context,
+        packet_type: u32,
+        data: &[u8],
+    ) -> parser::Result<()>;
+}
+
+struct HandlerImpl<S: PlayerState, M: prost::Message + Default, H: MessageHandler<S, M>> {
+    id: u32,
+    handler: Box<H>,
+    _phantom1: PhantomData<S>,
+    _phantom2: PhantomData<M>,
+}
+
+impl<S: PlayerState, M: prost::Message + Default, H: MessageHandler<S, M>> HandlerImpl<S, M, H> {
+    pub fn new(id: u32, handler: H) -> Box<Self> {
+        Box::new(Self {
+            id,
+            handler: Box::new(handler),
+            _phantom1: PhantomData,
+            _phantom2: PhantomData,
+        })
+    }
+}
+
+impl<S: PlayerState, M: prost::Message + Default, H: MessageHandler<S, M>> Handler<S>
+    for HandlerImpl<S, M, H>
+{
+    fn handle(
+        &mut self,
+        state: &mut S,
+        ctx: &Context,
+        packet_type: u32,
+        data: &[u8],
+    ) -> parser::Result<()> {
+        if packet_type != self.id {
+            return Ok(());
+        }
+
+        let msg = M::decode(data)?;
+
+        self.handler.handle(state, ctx, &msg)
+    }
+}
+
+pub struct Player {
+    pub slot: i32,
+    pub team_id: u8,
+    pub name: String,
+}
+
+pub struct HandlerVisitor<S: PlayerState> {
+    state: S,
+    handlers: Vec<Box<dyn Handler<S>>>,
+}
+
+impl<S: PlayerState + 'static> HandlerVisitor<S> {
+    pub fn with_state(state: S) -> Self {
+        Self {
+            state,
+            handlers: vec![],
+        }
+    }
+
+    pub fn with<M: prost::Message + Default + 'static, H: MessageHandler<S, M> + 'static>(
+        mut self,
+        id: u32,
+        handler: H,
+    ) -> Self {
+        self.handlers.push(HandlerImpl::new(id, handler));
+
+        self
+    }
+
+    pub fn state(&self) -> &S {
+        &self.state
+    }
+}
+
+impl<S: PlayerState> Visitor for &mut HandlerVisitor<S> {
+    fn on_packet(&mut self, ctx: &Context, packet_type: u32, data: &[u8]) -> parser::Result<()> {
+        for handler in self.handlers.iter_mut() {
+            handler.handle(&mut self.state, ctx, packet_type, data)?
+        }
+
+        Ok(())
+    }
+
+    fn on_entity(
+        &mut self,
+        ctx: &Context,
+        update_flags: usize,
+        update_type: crate::entities::UpdateType,
+        // TODO: include updated fields (list of field paths?)
+        entity: &crate::entities::Entity,
+    ) -> parser::Result<()> {
+        if let UpdateType::EnterPVS = update_type {
+            return Ok(())
+        }
+
+        let ser_key: u64 = fxhash::hash_bytes(b"CCitadelPlayerController");
+
+        let ser = entity.get_serializer().serializer_name.hash;
+
+        if ser != ser_key {
+            return Ok(());
+        }
+
+        let team_key: u64 = entities::make_field_key(&["m_iTeamNum"]);
+        let teamname_key: u64 = entities::make_field_key(&["m_szTeamname"]);
+        let playername_key: u64 = entities::make_field_key(&["m_iszPlayerName"]);
+        let playerslot_key: u64 = entities::make_field_key(&["m_unLobbyPlayerSlot"]);
+
+        if let (
+            Some(FieldValue::U8(team_id)),
+            Some(FieldValue::String(name)),
+            Some(FieldValue::U32(slot)),
+        ) = (
+            entity.get_value(&team_key),
+            entity.get_value(&playername_key),
+            entity.get_value(&playerslot_key),
+        ) {
+            self.state.set_player(Player {
+                slot: *slot as i32,
+                team_id: *team_id,
+                name: name.to_string(),
+            });
+        }
+        Ok(())
+    }
+}
--- examples/herokilled/src/main.rs
+++ examples/herokilled/src/main.rs
@@ -1,13 +1,14 @@
+use handler::{HandlerVisitor, Player, PlayerState};
 use haste::{
     entities::{self, Entity},
     fieldvalue::FieldValue,
-    parser::{self, Context, Parser, Visitor},
-    protos::{self, prost::Message},
+    parser::{self, Context, Parser},
+    protos::{self, CCitadelUserMsgChatMsg, CCitadelUserMsgHeroKilled},
     stringtables::StringTable,
 };
-use std::{fs::File, io::BufReader};
+use std::{collections::HashMap, fs::File, io::BufReader};
 
-struct MyVisitor;
+mod handler;
 
 fn get_entity_name<'a>(entity: &'a Entity, entity_names: &'a StringTable) -> Option<&'a str> {
     let name_si_key = entities::make_field_key(&["m_pEntity", "m_nameStringableIndex"]);
@@ -15,38 +16,79 @@ fn get_entity_name<'a>(entity: &'a Entity, entity_names: &'a StringTable) -> Opt
         return None;
     };
 
-    let Some((_, name_st_item)) = entity_names.items().find(|(i, _)| i.eq(&name_si)) else {
-        return None;
-    };
+    let (_, name_st_item) = entity_names.items().find(|(i, _)| i.eq(&name_si))?;
 
-    let Some(raw_string) = name_st_item.string.as_ref() else {
-        return None;
-    };
+    let raw_string = name_st_item.string.as_ref()?;
 
     std::str::from_utf8(raw_string).ok()
 }
 
-impl Visitor for MyVisitor {
-    fn on_packet(&mut self, ctx: &Context, packet_type: u32, data: &[u8]) -> parser::Result<()> {
-        if packet_type == protos::CitadelUserMessageIds::KEUserMsgHeroKilled as u32 {
-            let msg = protos::CCitadelUserMsgHeroKilled::decode(data)?;
+#[derive(Default)]
+struct Score {
+    kills: usize,
+    deaths: usize,
+}
 
-            let entities = ctx.entities().unwrap();
+#[derive(Default)]
+struct State {
+    hero_scores: HashMap<String, Score>,
+    players: HashMap<i32, Player>,
+}
 
-            let string_tables = ctx.string_tables().unwrap();
-            let entity_names = string_tables.find_table("EntityNames").unwrap();
-
-            let scorer = entities.get(&msg.entindex_scorer()).unwrap();
-            let scorer_name = get_entity_name(scorer, entity_names).unwrap();
-
-            let victim = entities.get(&msg.entindex_victim()).unwrap();
-            let victim_name = get_entity_name(victim, entity_names).unwrap();
-
-            println!("{} killed {}", scorer_name, victim_name);
-        }
-
-        Ok(())
+impl PlayerState for State {
+    fn set_player(&mut self, player: Player) {
+        self.players.insert(player.slot, player);
     }
+
+    fn get_player(&self, slot: &i32) -> Option<&Player> {
+        self.players.get(slot)
+    }
+}
+
+fn hero_killed(
+    state: &mut State,
+    ctx: &Context,
+    msg: &CCitadelUserMsgHeroKilled,
+) -> parser::Result<()> {
+    let entities = ctx.entities().unwrap();
+
+    let string_tables = ctx.string_tables().unwrap();
+    let entity_names = string_tables.find_table("EntityNames").unwrap();
+
+    let scorer = entities.get(&msg.entindex_scorer()).unwrap();
+    let scorer_name = get_entity_name(scorer, entity_names).unwrap();
+
+    let victim = entities.get(&msg.entindex_victim()).unwrap();
+    let victim_name = get_entity_name(victim, entity_names).unwrap();
+
+    println!("{} killed {}", scorer_name, victim_name);
+
+    state
+        .hero_scores
+        .entry(scorer_name.to_string())
+        .or_default()
+        .kills += 1;
+    state
+        .hero_scores
+        .entry(victim_name.to_string())
+        .or_default()
+        .deaths += 1;
+
+    Ok(())
+}
+
+fn chat(state: &mut State, ctx: &Context, msg: &CCitadelUserMsgChatMsg) -> parser::Result<()> {
+    let player = state
+        .get_player(&msg.player_slot())
+        .ok_or(parser::Error::from("unknown player"))?;
+    let channel = if msg.all_chat() {
+        format!("(all ) {}>", player.name)
+    } else {
+        format!("({}  ) {}>", player.team_id, player.name)
+    };
+    println!("{} {}", channel, msg.text());
+
+    Ok(())
 }
 
 type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
@@ -61,6 +103,24 @@ fn main() -> Result<()> {
 
     let file = File::open(filepath.unwrap())?;
     let buf_reader = BufReader::new(file);
-    let mut parser = Parser::from_reader_with_visitor(buf_reader, MyVisitor)?;
-    parser.run_to_end()
+    let state = State::default();
+    let mut visitor = HandlerVisitor::with_state(state)
+        .with(
+            protos::CitadelUserMessageIds::KEUserMsgHeroKilled as u32,
+            hero_killed,
+        )
+        .with(protos::CitadelUserMessageIds::KEUserMsgChatMsg as u32, chat);
+    let mut parser = Parser::from_reader_with_visitor(buf_reader, &mut visitor)?;
+    parser.run_to_end()?;
+
+    println!();
+
+    for (hero, score) in visitor.state().hero_scores.iter() {
+        println!(
+            "{} got {} kills and died {} times",
+            hero, score.kills, score.deaths
+        );
+    }
+
+    Ok(())
 }
