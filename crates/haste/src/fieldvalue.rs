a58c62432af7b4172efb7e05a36dd54c0121472f
blukai <20866892+blukai@users.noreply.github.com>
2024-09-08 01:15:25 +0200

    impl TryInto for FieldValue + refactor Debug and Display impls

--- crates/haste/src/fieldvalue.rs
+++ crates/haste/src/fieldvalue.rs
@@ -1,20 +1,4 @@
-// from public/dt_common.h
-//
-// typedef enum
-// {
-// 	DPT_Int=0,
-// 	DPT_Float,
-// 	DPT_Vector,
-// 	DPT_VectorXY, // Only encodes the XY of a vector, ignores Z
-// 	DPT_String,
-// 	DPT_Array,	// An array of the base types (can't be of datatables).
-// 	DPT_DataTable,
-// #if 0 // We can't ship this since it changes the size of DTVariant to be 20 bytes instead of 16 and that breaks MODs!!!
-// 	DPT_Quaternion,
-// #endif
-// 	DPT_Int64,
-// 	DPT_NUMSendPropTypes
-// } SendPropType;
+// NOTE: looking into public/dt_common.h might help to get more ideas about field value thing.
 
 // NOTE: Clone derive is needed here because Entity in entities.rs needs to be
 // clonable which means that all members of it also should be clonable.
@@ -24,20 +8,16 @@ pub enum FieldValue {
     I16(i16),
     I32(i32),
     I64(i64),
-
     U8(u8),
     U16(u16),
     U32(u32),
     U64(u64),
-
     Bool(bool),
     F32(f32),
-
     Vector([f32; 3]),
     Vector2D([f32; 2]),
     Vector4D([f32; 4]),
     QAngle([f32; 3]),
-
     // TODO: array backed string
     //
     // NOTE: for example smol_str does not fit well because its default size
@@ -45,56 +25,100 @@ pub enum FieldValue {
     String(Box<str>),
 }
 
-// ----
+#[derive(Debug, thiserror::Error)]
+#[error("tried to convert a field value into something that it isn't")]
+pub struct FieldValueTryIntoError(());
 
-impl std::fmt::Display for FieldValue {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+macro_rules! impl_try_into {
+    ($($variant:ident => $ty:ty),+) => {
+        $(
+            impl TryInto<$ty> for &FieldValue {
+                type Error = FieldValueTryIntoError;
+
+                fn try_into(self) -> Result<$ty, Self::Error> {
+                    match self {
+                        FieldValue::$variant(value) => Ok(*value),
+                        _ => Err(FieldValueTryIntoError(())),
+                    }
+                }
+            }
+        )+
+    }
+}
+
+impl_try_into! {
+    I8 => i8,
+    I16 => i16,
+    I32 => i32,
+    I64 => i64,
+    U8 => u8,
+    U16 => u16,
+    U32 => u32,
+    U64 => u64,
+    Bool => bool,
+    F32 => f32,
+    Vector2D => [f32; 2],
+    Vector4D => [f32; 4]
+}
+
+impl TryInto<[f32; 3]> for &FieldValue {
+    type Error = FieldValueTryIntoError;
+
+    fn try_into(self) -> Result<[f32; 3], Self::Error> {
         match self {
-            Self::I8(value) => f.write_fmt(format_args!("{:?}", value)),
-            Self::I16(value) => f.write_fmt(format_args!("{:?}", value)),
-            Self::I32(value) => f.write_fmt(format_args!("{:?}", value)),
-            Self::I64(value) => f.write_fmt(format_args!("{:?}", value)),
-
-            Self::U8(value) => f.write_fmt(format_args!("{:?}", value)),
-            Self::U16(value) => f.write_fmt(format_args!("{:?}", value)),
-            Self::U32(value) => f.write_fmt(format_args!("{:?}", value)),
-            Self::U64(value) => f.write_fmt(format_args!("{:?}", value)),
-
-            Self::Bool(value) => f.write_fmt(format_args!("{:?}", value)),
-            Self::F32(value) => f.write_fmt(format_args!("{:?}", value)),
-
-            Self::Vector(value) => f.write_fmt(format_args!("{:?}", value)),
-            Self::Vector2D(value) => f.write_fmt(format_args!("{:?}", value)),
-            Self::Vector4D(value) => f.write_fmt(format_args!("{:?}", value)),
-            Self::QAngle(value) => f.write_fmt(format_args!("{:?}", value)),
-
-            Self::String(value) => f.write_fmt(format_args!("{:?}", value)),
+            FieldValue::Vector(value) | FieldValue::QAngle(value) => Ok(*value),
+            _ => Err(FieldValueTryIntoError(())),
         }
     }
 }
 
-impl std::fmt::Debug for FieldValue {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+impl TryInto<String> for &FieldValue {
+    type Error = FieldValueTryIntoError;
+
+    fn try_into(self) -> Result<String, Self::Error> {
         match self {
-            Self::I8(value) => f.write_fmt(format_args!("I8({:?})", value)),
-            Self::I16(value) => f.write_fmt(format_args!("I16({:?})", value)),
-            Self::I32(value) => f.write_fmt(format_args!("I32({:?})", value)),
-            Self::I64(value) => f.write_fmt(format_args!("I64({:?})", value)),
-
-            Self::U8(value) => f.write_fmt(format_args!("U8({:?})", value)),
-            Self::U16(value) => f.write_fmt(format_args!("U16({:?})", value)),
-            Self::U32(value) => f.write_fmt(format_args!("U32({:?})", value)),
-            Self::U64(value) => f.write_fmt(format_args!("U64({:?})", value)),
-
-            Self::Bool(value) => f.write_fmt(format_args!("Bool({:?})", value)),
-            Self::F32(value) => f.write_fmt(format_args!("F32({:?})", value)),
-
-            Self::Vector(value) => f.write_fmt(format_args!("Vector({:?})", value)),
-            Self::Vector2D(value) => f.write_fmt(format_args!("Vector2D({:?})", value)),
-            Self::Vector4D(value) => f.write_fmt(format_args!("Vector4D({:?})", value)),
-            Self::QAngle(value) => f.write_fmt(format_args!("QAngle({:?})", value)),
-
-            Self::String(value) => f.write_fmt(format_args!("String({:?})", value)),
+            FieldValue::String(value) => Ok(value.to_string()),
+            _ => Err(FieldValueTryIntoError(())),
         }
     }
+}
+
+macro_rules! impl_debug {
+    ($($variant:ident),+) => {
+        impl std::fmt::Debug for FieldValue {
+            fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+                match self {
+                    $(Self::$variant(value) => f.debug_tuple(stringify!($variant)).field(value).finish(),)+
+                }
+            }
+        }
+    };
+}
+
+impl_debug! {
+    I8, I16, I32, I64,
+    U8, U16, U32, U64,
+    Bool, F32,
+    Vector, Vector2D, Vector4D, QAngle,
+    String
+}
+
+macro_rules! impl_display {
+    ($($variant:ident),+) => {
+        impl std::fmt::Display for FieldValue {
+            fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+                match self {
+                    $(Self::$variant(value) => write!(f, "{:?}", value),)+
+                }
+            }
+        }
+    };
+}
+
+impl_display! {
+    I8, I16, I32, I64,
+    U8, U16, U32, U64,
+    Bool, F32,
+    Vector, Vector2D, Vector4D, QAngle,
+    String
 }
